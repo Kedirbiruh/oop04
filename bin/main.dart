1f30ca5a68b3e422d107ae3600bee8ec3d9859bc
Kedirbiruh <kedirbiruh1114@gmail.com>
2025-09-28 14:46:22 +0200

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:oop04/models.dart';
+
+void main() {
+  var t1 = Participant(
+    firstName: 'Abel',
+    lastName: 'Müller',
+    age: 30,
+    birthDate: DateTime(2000, 6, 9),
+    finalGrade: 1.3,
+    status: 'student',
+  );
+
+  var t2 = Participant(firstName: 'Sahra', lastName: 'Sadiq',  age: 25,);
+
+  var t3 = Participant(
+    firstName: 'Kai',
+    lastName: 'Raucher',
+    finalGrade: 2.3,
+    age: 28, 
+    // finalGrade: 10.0,
+  );
+
+  t1.printInfo();
+  t2.printInfo();
+  print(t3);
+}
---
+++ lib/models.dart
@@ -0,0 +1,48 @@
+class Participant {
+  String firstName;
+  String lastName;
+  int age;
+  DateTime? birthDate;
+  double? finalGrade;
+  double? weight;
+  double? height;
+  String? status;
+
+  Participant({
+    required this.firstName,
+    required this.lastName,
+    required this.age,
+    this.birthDate,
+    this.finalGrade,
+    this.weight,
+    this.height,
+    this.status = 'Aktiv',
+  });
+
+  // Getter: zusammengesetzter Wert
+  String get fullName => '$firstName $lastName';
+
+  // Beispiel-Setter mit Validierung (optional)
+  set grade(double? value) {
+    if (value != null && (value < 0 || value > 6)) {
+      throw ArgumentError('Note muss zwischen 0 und 6 liegen');
+    }
+    finalGrade = value;
+  }
+
+  void printInfo() {
+    print('Name: $fullName');
+    print('Status: $status');
+    print('Alter: $age ? "nicht angegeben"');
+    print('Geburtstag: ${birthDate?.toIso8601String() ?? "nicht angegeben"}');
+    print(
+      'Abschlussnote: ${finalGrade?.toStringAsFixed(2) ?? "nicht vorhanden"}',
+    );
+    print("Hi, ich heiße $firstName, bin $age Jahre alt.");
+  }
+
+  @override
+  String toString() {
+    return 'Teilnehmer(fullName: $fullName, role: $status, age: ${age ?? "?"})';
+  }
+}
